360a9eb7df1ef7f7573e78e8a3830d71b11290fa
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-02-09 17:18:20 -0500

    Add files via upload

---
+++ pubchem_pugrestapi.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 14:57:33 2023
+
+@author: Tim
+"""
+
+import time 
+import requests
+
+url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'
+url_input = 'compound/cid/'
+cids = []
+
+for i in range(1,101):
+    cids.append(i)
+    
+url_output = '/property/CanonicalSMILES/txt'
+f = open('trial_mols.sdf', 'w')
+for cid in range(len(cids)):
+    req_list = [url, url_input, str(cids[cid]), url_output]
+    req = ''.join(req_list)
+    res = requests.get(req)
+    f.write(res.text)
+    time.sleep(0.2)
+   
+f.close()

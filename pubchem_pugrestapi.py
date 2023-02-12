ecb5c36b6264ac9a327ea4bcfa20ae82253f4b87
Tim Dunn <86075550+tdunn1@users.noreply.github.com>
2023-02-12 16:01:49 -0500

    Add files via upload

---
+++ pubchem_pugrestapi.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 14:57:33 2023
+
+@author: Tim
+"""
+
+import time 
+import requests
+from rdkit import Chem
+from rdkit.Chem.rdmolfiles import SDWriter
+import pdb
+
+url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/'
+url_input = 'compound/cid/'
+cids = []
+
+for i in range(1,101):
+    cids.append(i)
+    
+mol_list = []
+url_output = '/property/CanonicalSMILES/txt'
+for cid in range(len(cids)):
+    req_list = [url, url_input, str(cids[cid]), url_output]
+    req = ''.join(req_list)
+    res = requests.get(req)
+    res = res.text
+    mol = Chem.MolFromSmiles(res)
+    mol_list.append(mol)
+    time.sleep(0.2)
+   
+with SDWriter('trial_mols.sdf') as w:
+    for mol in mol_list:
+        w.write(mol)
+
+# mol_list = ['C1=CC=C(C=C1)C(=O)O', 'CC1=CC=CC=C1']
+# with SDWriter('sample_mols.sdf') as w:
+#     for smi in mol_list:
+#         mol = Chem.MolFromSmiles(smi)
+#         w.write(mol)
+        
